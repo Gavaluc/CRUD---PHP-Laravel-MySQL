a9581368f1d2df2a001f84260bae8f6f72bf4c2c
Lucas Gava <lucashenriquegava@outlook.com>
2022-11-19 01:06:59 -0300

    first one

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\UsuariosController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('usuarios', [UsuariosController::class, 'index']);
+Route::get('usuarios/novo', [UsuariosController::class, 'create']);
+Route::get('usuarios/editar/{id}', [UsuariosController::class, 'edit']);
+Route::get('usuarios/excluir/{id}', [UsuariosController::class, 'destroy']);
+Route::get('usuarios/{id}', [UsuariosController::class, 'show']);
+Route::post('usuarios/gravar/{id}', [UsuariosController::class, 'update']);
+Route::post('usuarios/salvar', [UsuariosController::class, 'store']);
+
+//Route::get('home', [HomeController::class, 'index']);
+//Route::get('users', [HomeController::class, 'users']);
+//Route::get('users-db', [HomeController::class, 'usersDatabase']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
